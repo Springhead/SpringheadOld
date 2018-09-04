b3304f115054a1b83440b358aef4a0788a603a29
DailyBuild <kanehori@haselab.net>
2018-09-04 12:27:33 +0900

    dailybuild (unix): 環境変数をsourceしようとしたがうまくいかないので取り敢えず・・・

--- core/test/DailyBuild.sh
+++ core/test/DailyBuild.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
 # Set test environment.
-
-ENVDEF="SprPath.sh"
-
-if [ -f ${ENVDEF} ]; then
-	source ${ENVDEF}
-fi
+export SPR_PYTHON=/usr/local/bin/python
+export SPR_TOP=$HOME/Project/Springhead
+export SPR_DEPENDENCY=$SPR_TOP/dependency
+export SPR_EXTLIBS=$HOME/ExtLibs
+export SPR_BOOST_INC=$SPR_EXTLIBS/boost-numeric-bindings-R86799
+export SPR_GL_INC=$SPR_DEPENDENCY/src
 
 # Use -A option to skip following steps.
 #	Update Springhead to HEAD status - git pull.
