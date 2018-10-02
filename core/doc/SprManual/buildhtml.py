88d688fe5020c1d8aa1110e70bc308c65fdd09e6
DailyBuild <kanehori@haselab.net>
2018-10-02 11:04:55 +0900

    buildhtml.py for python 3.5 (sorry, plasTeX do not work)

--- core/doc/SprManual/buildhtml.py
+++ core/doc/SprManual/buildhtml.py
@@ -11,13 +11,14 @@
 #
 # ----------------------------------------------------------------------
 #  VERSION:
-#	Ver 1.0  2018/09/20 F.Kanehori	First version.
+#	Ver 1.0  2018/09/25 F.Kanehori	First version.
 # ======================================================================
 version = 1.0
 
 import sys
 import os
 import glob
+import subprocess
 from optparse import OptionParser
 
 # ----------------------------------------------------------------------
@@ -28,52 +29,144 @@ web_host = 'haselab.net'
 web_user = 'demo'
 
 # ----------------------------------------------------------------------
-#  Import Springhead python library.
-#
-sys.path.append('../../src/RunSwig')
-from FindSprPath import *
-spr_path = FindSprPath(prog)
-libdir = spr_path.abspath('pythonlib')
-sys.path.append(libdir)
-from FileOp import *
-from Proc import *
-from Util import *
-from Error import *
-
-# ----------------------------------------------------------------------
 #  External tools.
 #
 sed = 'sed'
 nkf = 'nkf'
-python = 'python'
+python = 'c:/python35/python'
 plastex = 'c:/Python35/Scripts/plastex'
 
 # ----------------------------------------------------------------------
 #  Helper methods
 #
-#  Copy all files.
+#  Platform now running?
+def is_unix():
+	return True if os.name == 'posix' else False
+
+#  Execute command.
+def execute(cmnd, stdin=None, stdout=None, stderr=None, shell=None):
+	fd = [ pipe_open(stdin, 'r', dry_run),
+	       pipe_open(stdout, 'w', dry_run),
+	       pipe_open(stderr, 'w', dry_run) ]
+	if dry_run:
+		print('EXEC: %s' % cmnd)
+		return 0
+	if shell is None:
+		shell = True if is_unix() else False
+	if verbose:
+		print('exec: %s' % cmnd)
+	proc = subprocess.Popen(cmnd,
+				stdin=fd[0], stdout=fd[1], stderr=fd[2],
+				shell=shell)
+	return proc
+
+def pipe_open(file, mode, dry_run):
+	if dry_run:
+		return None
+	if not isinstance(file, str):
+		return file
+	try:
+		f = open(file, mode)
+	except IOError as err:
+		f = None
+	return f
+
+#  Wait for process termination.
+def wait(proc):
+	rc = proc.wait()
+	if not is_unix():
+		rc = -(rc & 0b1000000000000000) | (rc & 0b0111111111111111)
+	return rc
+
+#  Remove tree.
+def remove_tree(top, dry_run=False, verbose=0):
+	if is_unix():
+		cmnd = '/bin/rm -rf %s' % top
+	else:
+		cmnd = 'rd /S /Q %s' % top
+	if dry_run or verbose:
+		print('remove_tree: %s' % top)
+		if dry_run:
+			return 0
+	rc = wait(execute(cmnd, shell=True))
+	return rc
+
+#  Copy file(s).
+def cp(src, dst, dry_run=False, verbose=0):
+	if os.path.isdir(src) and os.path.isfile(dst):
+		msg = 'copying directory to plain file (%s to %s)' % (src, dst)
+		abort(msg)
+	if os.path.isfile(src) and os.path.isdir(dst):
+		msg = 'copying plain file to directory (%s to %s)' % (src, dst)
+		abort(msg)
+	if dry_run:
+		print('cp: %s %s' % (src, dst))
+		if dry_run:
+			return 0
+	if os.path.isdir(src):
+		if verbose:
+			print('cp: %s -> %s/%s' % (f, dst, f))
+		if is_unix():
+			## NEED IMPLEMENT
+			pass
+		else:
+			cmnd = 'xcopy /I /E /S /Y /Q %s %s' % (src, dst)
+			rc = wait(execute(cmnd, shell=True))
+	else:
+		if verbose:
+			print('cp: %s -> %s' % (f, dst))
+		cmnd = '%s %s %s' % (cmndname('cp'), src, dst)
+		rc = wait(execute(cmnd, shell=True))
+	return rc
+
 def copy_all(src, dst, dry_run=False, verbose=0):
 	if verbose:
 		print('  clearing "%s"' % dst)
-	fop = FileOp(info=1, dry_run=dry_run, verbose=verbose)
-	fop.rm('%s/*' % dst, recurse=True)
+	cmnd = '%s %s/*' % (cmndname('rm'), dst)
+	wait(execute(cmnd))
 	#
 	os.chdir(src)
 	print('  copying "%s" to "%s"' % (src, dst))
 	names = os.listdir()
 	for name in names:
 		if os.path.isfile(name):
-			fop.cp(name, dst)
+			cmnd = '%s %s %s' % (cmndname('cp'), src, dst)
+			rc = wait(execute(cmnd))
+			if rc != 0:
+				break
 		elif os.path.isdir(name):
 			dst_dir = '%s/%s' % (dst, name)
-			fop.cp(name, dst_dir)
+			cmnd = '%s %s %s' % (cmndname('cp'), src, dst_dir)
+			rc = wait(execute(cmnd))
+			if rc != 0:
+				break
+	return rc
+
+#
+def pathconv(path):
+	return path.replace('/', os.sep)
+
+#  Command name.
+def cmndname(cmnd):
+	nametab = { 'cat':	['cat', 'type'],
+		    'cp':	['cp', 'copy'],
+		    'rm':	['rm', 'del'],
+		}
+	indx = 0 if is_unix() else 1
+	return nametab[cmnd][indx]
+
+#  Error process.
+def error(msg):
+	sys.stderr.write('%s\n' % msg)
+def abort(msg, exitcode=1):
+	error(msg)
+	sys.exit(exitcode)
 
 #  Show usage.
 def print_usage():
 	print()
 	cmnd = 'python %s.py --help' % prog
-	shell = False if Util.is_unix() else True
-	Proc().execute(cmnd, shell=shell).wait()
+	wait(execute(cmnd))
 	sys.exit(1)
 
 # ----------------------------------------------------------------------
@@ -103,7 +196,7 @@ if options.version:
 	print('%s: Version %s' % (prog, version))
 	sys.exit(0)
 if len(args) != 1:
-	Error(prog).error('incorrect number of arguments')
+	error('incorrect number of arguments')
 	print_usage()
 #
 wrkspace = options.wrkspace
@@ -113,107 +206,106 @@ dry_run = options.dry_run
 texmain = args[0]
 if texmain[-4:] != '.tex':
 	texmain += '.tex'
-#
-shell = True if Util.is_unix() else False
 
 # ----------------------------------------------------------------------
 #  Main process.
 #
 # ----------------------------------------------------------------------
-#  Create work space.
+#  Prepare work space.
 #
 if os.path.exists(wrkspace) and not os.path.isdir(wrkspace):
 	msg = '%s exists but not a directory' % wrkspace
-	Error(prog).abort(msg)
-os.makedirs(wrkspace, exist_ok=True)
-
-#  Clear work space.
-#
-fop = FileOp()
-fop.rm('%s/*' % wrkspace)
+	abort(msg)
+if os.path.exists(wrkspace):
+	rc = remove_tree(wrkspace, dry_run, verbose)
+	if rc != 0:
+		msg = 'clearing workspace failed'
+		abort(msg)
+if verbose:
+	print('making %s' % wrkspace)
+os.mkdir(wrkspace)
 
 #  Copy files to work space.
 #
 texsrcs = glob.glob('*.tex')
 texsrcs.extend(glob.glob('*.sty'))
 for f in texsrcs:
-	if Util.is_unix():
-		cmnd1 = 'cat %s' % f
+	cmnd1 = '%s %s' % (cmndname('cat'), f)
+	if is_unix():
 		cmnd2 = "%s -e 's/\{sourcecode\}/\{verbatim\}/'" % sed
 	else:
-		cmnd1 = 'type %s' % f
 		cmnd2 = '%s -e "s/{sourcecode}/{verbatim}/"' % sed
 	cmnd3 = '%s -w' % nkf
 	outf = '%s/%s' % (wrkspace, f)
 	if verbose:
 		print('converting %s' % f)
-	proc1 = Proc()
-	proc2 = Proc()
-	proc3 = Proc()
 	#print('cmnd1: %s' % cmnd1)
-	#print('cmnd2: %s' % cmnd2)
-	#print('cmnd3: %s' % cmnd3)
-	proc1.execute(cmnd1, stdout=Proc.PIPE, shell=True)
-	proc2.execute(cmnd2, stdin=proc1.proc.stdout,
-					stdout=Proc.PIPE, shell=shell)
-	proc3.execute(cmnd3, stdin=proc2.proc.stdout,
-					stdout=outf, shell=shell)
+	#print('cmnd2: %s' % cmnd2 )
+	#print('cmnd3: %s' % cmnd3 )
+	proc1 = execute(cmnd1, stdout=subprocess.PIPE, shell=True)
+	proc2 = execute(cmnd2, stdin=proc1.stdout,
+				stdout=subprocess.PIPE)
+	proc3 = execute(cmnd3, stdin=proc2.stdout,
+				stdout=outf)
 	proc1.wait()
 	proc2.wait()
 	rc = proc3.wait()
 	if rc != 0:
 		msg = 'file conversion failed: "%s"' % f
-		Error(prog).abort(msg)
+		abort(msg)
 #
 others = glob.glob('*.cls')
 others.extend(['en', 'fig'])
 for f in others:
-	rc = FileOp().cp(f, '%s/%s' % (wrkspace, f))
+	dst = pathconv('%s/%s' % (wrkspace, f))
+	rc = cp(f, dst, dry_run=dry_run, verbose=verbose)
 	if rc != 0:
 		msg = 'file copy failed: "%s"' % f
-		Error(prog).abort(msg)
+		abort(msg)
 #
 htmls = texmain.replace('.tex', '')
 imgsrc = 'images'
-imgdest = '%s/%s/images' % (wrkspace, htmls)
-rc = FileOp(verbose=verbose).cp(imgsrc, imgdest)
+imgdst = pathconv('%s/%s/images' % (wrkspace, htmls))
+rc = cp(imgsrc, imgdst, dry_run=dry_run, verbose=verbose)
 if rc != 0:
 	msg = 'file copy failed: "%s"' % imgsrc
-	Error(prog).abort(msg)
+	abort(msg)
 
 #  Generating htmls.
 #
 cwd = os.getcwd()
 os.chdir(wrkspace)
+opts = '--renderer=HTML5 --dir=main_html'
 cmnd = '%s %s %s' % (python, plastex, texmain)
-rc = Proc(verbose=verbose).execute(cmnd, shell=shell).wait()
+if verbose:
+	print('cwd: %s' % os.getcwd())
+	print('cmnd: %s' % cmnd)
+rc = wait(execute(cmnd))
 os.chdir(cwd)
 if rc != 0:
 	msg = 'generating html failed'
-	Error(prog).abort(msg)
+	abort(msg)
 #
-if not options.copy:
-	sys.exit(0)
-
-#  Copy generated files to web server.
-#
-fmdir = '.'
-todir = 'WWW/docroots/springhead/doc/SprManual'
-if Util.is_unix():
-	remote = '%s@%s:/home/%s' % (web_user, web_host, todir)
-	pkey = '%s/.ssh/id_rsa' % os.environ['HOME']
-	opts = '-i %s -o "StrictHostKeyChecking=no"' % pkey
-	cmnd = 'scp %s %s/* %s' % (opts, fmdir, remote)
-	print('## %s' % cmnd)
-	rc = proc.execute(cmnd, shell=True).wait()
-	if rc == 0:
-		print('cp %s -> %s:%s' % (fmdir, tohost, todir))
+if options.copy:
+	#  Copy generated files to web server.
+	#
+	fmdir = '.'
+	todir = 'WWW/docroots/springhead/doc/SprManual'
+	if Util.is_unix():
+		remote = '%s@%s:/home/%s' % (web_user, web_host, todir)
+		pkey = '%s/.ssh/id_rsa' % os.environ['HOME']
+		opts = '-i %s -o "StrictHostKeyChecking=no"' % pkey
+		cmnd = 'scp %s %s/* %s' % (opts, fmdir, remote)
+		print('## %s' % cmnd)
+		rc = wait(execute(cmnd))
+		if rc == 0:
+			print('cp %s -> %s:%s' % (fmdir, tohost, todir))
+		else:
+			print('cp %s failed' % fmdir)
 	else:
-		print('cp %s failed' % fmdir)
-else:
-	remote = '//%s/HomeDirs/%s' % (web_host, todir)
-	copy_all(fmdir, remote, dry_run=True)
-
+		remote = '//%s/HomeDirs/%s' % (web_host, todir)
+		copy_all(fmdir, remote, dry_run=True)
+#
 sys.exit(0)
 
 # end: buildhtml.py
