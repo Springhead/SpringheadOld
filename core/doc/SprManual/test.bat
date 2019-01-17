c6221e5ee496b2d63b37dc3088b2211b76acfc33
DailyBuild <kanehori@haselab.net>
2019-01-17 15:32:49 +0900

    SprManual: html special character については暫定版

--- core/doc/SprManual/test.bat
+++ core/doc/SprManual/test.bat
@@ -1,17 +1,39 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set CWD=%CD%
-if "%CWD:~-9%" equ "SprManual" (set OPT=)
-if "%CWD:~-3%" equ "tmp" (set OPT=-S)
+if "%1" equ "" (
+    echo %0: argument required
+    echo Usage: %0 { pdf ^| html }
+    exit /b
+)
+set ARGS=%2 %3 %4 %5 %6 %7 %8 %9
 
-if "%OPT%" equ "-S" (cd ..)
+set ADDPATH=.
+if "%1" equ "pdf"  (set ADDPATH=F:\C-Drive-Save\texlive\2014\bin\win32)
+if "%1" equ "html" (set ADDPATH=C:\texlive\2018\bin\win32)
+set path=%ADDPATH%;%path%
 
-echo test start at "%CD%"
-python buildhtml.py -v -E -K -R -c -t %OPT% %* main_html.tex
+if "%1" equ "pdf" (
+    cmd /c make clean
+    cmd /c make.bat
 
-if "%OPT%" equ "-S" (cd tmp)
-echo test end at "%CD%"
+) else if "%1" equ "html" (
+    set CWD=%CD%
+    if "!CWD:~-9!" equ "SprManual" (set OPT=)
+    if "!CWD:~-3!" equ "tmp" (set OPT=-S)
+
+    if "!OPT!" equ "-S" (cd ..)
+
+    echo test start at "%CD%"
+    python buildhtml.py -v -E -K -R -c -t !OPT! %ARGS% main_html.tex
+
+    if "!OPT!" equ "-S" (cd tmp)
+    echo test end at "%CD%"
+
+) else (
+    echo %0: invalid argument
+    echo Usage: %0 { pdf ^| html }
+)
 
 endlocal
 exit /b
