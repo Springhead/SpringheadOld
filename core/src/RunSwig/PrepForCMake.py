20d312b75e06e6532e94db6016f15bf9bf40f591
DailyBuild <kanehori@haselab.net>
2019-02-28 12:25:22 +0900

    CMake test (RunSwig)

--- core/src/RunSwig/PrepForCMake.py
+++ core/src/RunSwig/PrepForCMake.py
@@ -2,15 +2,18 @@
 # -*- coding: utf-8 -*-
 # ==============================================================================
 #  SYNOPSIS:
-#	python PrepForCMake.py
+#	python PrepForCMake.py src-dir dst-dir
+#	arguments:
+#	    src-dir:	'src' directory path on the original source tree.
+#	    dst-dir:	'src' directory path on the build tree.
 #
 #  DESCRIPTION:
 #	CMake を使用するにあたって必要な前処理を行なう。
 #
-#	(1) RunSwig を実行するにあたり、必要となるスクリプトファイル等が build
+#	(1) RunSwig を実行するにあたり、必要となるスクリプトファイル等がビルド
 #	    ディレクトリにないと実行できないので、予めコピーする。
 #
-#	(2) Build ディレクトリが src/build となることで各種の相対パスが異なって
+#	(2) ビルドディレクトリが <dst_dir> となることで各種の相対パスが変わって
 #	    くる。実行パスについては絶対パス化することで回避できるが、.i ファイ
 #	    ルには相対パスが直接記述されているため、パスの書き換えを行わないと
 #	    従来のシステムとは共存できない (CMakeしか使わないのであれば.iファイ
@@ -18,50 +21,29 @@
 #
 # ==============================================================================
 #  Version:
-#	Ver 1.0	 2019/02/26 F.Kanehori	First release version.
+#	Ver 1.0	 2019/02/28 F.Kanehori	First release version.
 # ==============================================================================
 version = 1.0
 
 import sys
 import os
 import glob
+import shutil
+import subprocess
 from optparse import OptionParser
 
 # ----------------------------------------------------------------------
 #  Constants
 #
-prog = sys.argv[0].replace(os.sep, '/').split('/')[-1].split('.')[0]
-
-# ----------------------------------------------------------------------
-#  Import Springhead python library.
-#
-from FindSprPath import *
-spr_path = FindSprPath(prog)
-libdir = spr_path.abspath('pythonlib')
-sys.path.append(libdir)
-from Error import *
-from Util import *
-from Proc import *
-from FileOp import *
-
-# ----------------------------------------------------------------------
-#  Directories
-#
-sprtop = spr_path.abspath()
-##bindir = spr_path.relpath('bin')
-srcdir = spr_path.abspath('src')
-RSdir = '%s/%s' % (srcdir, 'RunSwig')
-FOdir = '%s/%s' % (srcdir, 'Foundation')
-FWdir = '%s/%s' % (srcdir, 'Framework')
+prog = os.path.basename(sys.argv[0]).split('.')[0]
 
 # ---------------------------------------------------------------------
 #  Options
 #
-usage = 'Usage: %prog [options]'
+usage = 'Usage: %prog [options] src-dir dst-dir\n'\
+	+ '\tsrc-dir: src directory on the original source tree\n'\
+	+ '\tdst-dir: src directory on the build tree'
 parser = OptionParser(usage = usage)
-parser.add_option('-b', '--build-dir', dest='build_dir',
-			action='store', default='build',
-                        help='build (working) directory')
 parser.add_option('-v', '--verbose', dest='verbose',
 			action='count', default=0,
                         help='set verbose count')
@@ -76,54 +58,86 @@ parser.add_option('-V', '--version', dest='version',
 if options.version:
         print('%s: Version %s' % (prog, version))
         sys.exit(0)
+if len(args) != 2:
+	print('%s: incorrect number of arguments\n' % prog)
+	subprocess.Popen('python %s.py -h' % prog, shell=True).wait()
+	sys.exit(1)
 
-build_dir = options.build_dir
+srcdir = args[0].replace('\\', '/')
+dstdir = args[1].replace('\\', '/')
 verbose = options.verbose
+if verbose:
+	print('src-dir: %s' % srcdir)
+	print('dst-dir: %s' % dstdir)
 
 # ----------------------------------------------------------------------
 #  Directories
 #
-blddir = '%s/%s' % (srcdir, build_dir)
-RS_blddir = '%s/%s' % (blddir, 'RunSwig')
-FO_blddir = '%s/%s' % (blddir, 'Foundation')
-FW_blddir = '%s/%s' % (blddir, 'Framework')
+RS_srcdir = '%s/RunSwig' % srcdir
+FO_srcdir = '%s/Foundation' % srcdir
+FW_srcdir = '%s/Framework' % srcdir
+RS_dstdir = '%s/RunSwig' % dstdir
+FO_dstdir = '%s/Foundation' % dstdir
+FW_dstdir = '%s/Framework' % dstdir
+
+# ----------------------------------------------------------------------
+#  Methods
+#
+def expand_filenames(names):
+	fnames = []
+	for name in names:
+		fnames.extend(glob.glob(name))
+	return fnames
 
 # ----------------------------------------------------------------------
 #  Main process
 #
 cwd = os.getcwd()
-Fop = FileOp(verbose=0)
 
 # (1) RunSwig にあるスクリプトファイル等を作業場所にコピーする。
 #
-os.chdir(RSdir)
-Fop.cp('*.py', RS_blddir)
-Fop.cp('*.bat', RS_blddir)
-Fop.cp('*.projs', RS_blddir)
-Fop.cp('Makefile*', RS_blddir)
+os.chdir(RS_srcdir)
+fnames = expand_filenames(['*.py', '*.bat', '*.projs', 'Makefile*'])
+for fname in fnames:
+	if verbose:
+		print('  copy %s -> %s' % (fname, RS_dstdir))
+	shutil.copy(fname, RS_dstdir)
 #
 pythonlib = 'pythonlib'
-Fop.makedirs(pythonlib, exist_ok=True)
+dstdir = '%s/%s' % (RS_dstdir, pythonlib)
+#os.makedirs(dstdir, exist_ok=True)	## ??
+if not os.path.exists(dstdir):
+	os.mkdir(dstdir)
 os.chdir(pythonlib)
-Fop.cp('*', '%s/%s' % (RS_blddir, pythonlib))
-os.chdir(cwd)
+fnames = expand_filenames(['*.py'])
+for fname in fnames:
+	if verbose:
+		print('  copy %s/%s -> %s' % (pythonlib, fname, dstdir))
+	shutil.copy(fname, dstdir)
 
 # (2) Foundation にあるスクリプトファイルを作業場所にコピーする
 #     Foundation にある.iファイルを変更して作業場所に書き出す
 #
-os.chdir(FOdir)
-Fop.cp('RunSwig.py', FO_blddir)
-Fop.cp('ScilabSwig.py', FO_blddir)
-cmnd = 'python %s/replace.py' % RSdir
-args = '-o %s/Scilab.i Scilab.i ../../include=../../../include' % FO_blddir
-stat = Proc().execute('%s %s' % (cmnd, args)).wait()
+os.chdir(FO_srcdir)
+for fname in ['RunSwig.py', 'ScilabSwig.py']:
+	if verbose:
+		print('  copy %s -> %s' % (fname, FO_dstdir))
+	shutil.copy(fname, FO_dstdir)
+
+cmnd = 'python %s/replace.py' % RS_srcdir
+args = '-o %s/Scilab.i Scilab.i ../../include=../../../include' % FO_dstdir
+stat = subprocess.Popen('%s %s' % (cmnd, args)).wait()
 if stat != 0:
-	Error(prog).abort('rewrite "Scilab.i" failed')
+	print('%s: Error: rewrite "Scilab.i" failed' % prog)
+	sys.exit(1)
 
 # (3) Framework にあるスクリプトファイルを作業場所にコピーする
 #
-os.chdir(FWdir)
-Fop.cp('RunSwigFramework.py', FW_blddir)
+os.chdir(FW_srcdir)
+for fname in ['RunSwigFramework.py']:
+	if verbose:
+		print('  copy %s -> %s' % (fname, FW_dstdir))
+	shutil.copy(fname, FW_dstdir)
 
 #  以上
 #
