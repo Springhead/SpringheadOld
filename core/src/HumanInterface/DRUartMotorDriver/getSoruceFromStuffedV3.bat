e866ae54404c912bf8ae3afd0e67a4a283057d63
hase <hasevr@gmail.com>
2020-01-17 10:10:58 +0900

    use hard link instead of copy

--- core/src/HumanInterface/DRUartMotorDriver/getSoruceFromStuffedV3.bat
+++ core/src/HumanInterface/DRUartMotorDriver/getSoruceFromStuffedV3.bat
@@ -3,14 +3,17 @@ mkdir WROOM
 mkdir WROOM\main
 mkdir WROOM\main\softRobot
 mkdir PCRobokey
-copy ..\..\..\..\..\\StuffedV3\PCRobokey\commandId.cs PCRobokey
-copy ..\..\..\..\..\\StuffedV3\PIC\commandCommon.h PIC
-copy ..\..\..\..\..\\StuffedV3\PIC\commandTemplate.h PIC
-copy ..\..\..\..\..\\StuffedV3\PIC\control.h PIC
-copy ..\..\..\..\..\\StuffedV3\PIC\env.h PIC
-copy ..\..\..\..\..\\StuffedV3\PIC\fixed.h PIC
-copy ..\..\..\..\..\\StuffedV3\WROOM\main\softRobot\CommandWROOM.h WROOM\main\softRobot
-copy ..\..\..\..\..\\StuffedV3\WROOM\main\softRobot\Board.h WROOM\main\softRobot
-copy ..\..\..\..\..\\StuffedV3\WROOM\main\softRobot\BoardBase.h WROOM\main\softRobot
-copy ..\..\..\..\..\\StuffedV3\WROOM\main\softRobot\Board.cpp WROOM\main\softRobot
+del /y Robokey\*
+del /y WROOM\main\softRobot\*
+del /y PIC\*
+ln ..\..\..\..\..\\StuffedV3\PCRobokey\commandId.cs PCRobokey\commandId.cs
+ln ..\..\..\..\..\\StuffedV3\PIC\commandCommon.h PIC\commandCommon.h
+ln ..\..\..\..\..\\StuffedV3\PIC\commandTemplate.h PIC\commandTemplate.h
+ln ..\..\..\..\..\\StuffedV3\PIC\control.h PIC\control.h
+ln ..\..\..\..\..\\StuffedV3\PIC\env.h PIC\env.h
+ln ..\..\..\..\..\\StuffedV3\PIC\fixed.h PIC\fixed.h
+ln ..\..\..\..\..\\StuffedV3\WROOM\main\softRobot\CommandWROOM.h WROOM\main\softRobot\CommandWROOM.h
+ln ..\..\..\..\..\\StuffedV3\WROOM\main\softRobot\Board.h WROOM\main\softRobot\Board.h
+ln ..\..\..\..\..\\StuffedV3\WROOM\main\softRobot\BoardBase.h WROOM\main\softRobot\BoardBase.h
+ln ..\..\..\..\..\\StuffedV3\WROOM\main\softRobot\Board.cpp WROOM\main\softRobot\Board.cpp
 
