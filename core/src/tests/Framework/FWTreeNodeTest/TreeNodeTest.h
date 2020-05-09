8931ac6995abae95618cb0c4587e2725f56012af
hase <hasevr@gmail.com>
2020-05-09 15:02:15 +0900

    Add FWTreeNodeTest to test treenode and PD control.

---
+++ core/src/tests/Framework/FWTreeNodeTest/TreeNodeTest.h
@@ -0,0 +1,26 @@
+﻿#ifndef TreeNodeTest_H
+#define TreeNodeTest_H
+
+#include <Springhead.h>
+#include <Framework/SprFWApp.h>
+#include <Base/Combination.h>
+#include <Collision/CDDetectorImp.h>
+#include <Physics/PHSolid.h>
+
+using namespace Spr;
+
+
+class TreeNodeTest : public FWApp{
+public:
+	// 剛体に使う変数
+	std::vector<PHSolidIf*> links[2];
+	std::vector<PHBallJointIf*> joints[2];
+	//デバック表示に使う変数
+	void Init(int argc, char* argv[]);		
+	void BuildScene();
+	void UserFunc();
+	void Keyboard(int key, int x, int y);
+}; 
+extern TreeNodeTest ttest;
+
+#endif
---
+++ core/src/tests/Framework/FWTreeNodeTest/main.cpp
@@ -0,0 +1,7 @@
+﻿#include "TreeNodeTest.h"
+
+int __cdecl main(int argc, char* argv[]){
+	ttest.Init(argc, argv);
+	ttest.StartMainLoop();
+	return 0;
+}
